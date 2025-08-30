bc604eb540e3b86540b1893029c1178c54724200
Bob Stark <bob@datagotchi.net>
2025-08-29 20:09:43 -0400

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Use playwright fixtures instead of shared variables in before/after each calls

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
-  globalSetup: join(__dirname, "global-setup.ts"),
+  globalSetup: join(__dirname, "src/e2e-tests/global-setup.ts"),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
